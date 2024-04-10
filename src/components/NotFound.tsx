0ee06ea5f7f72f1173ef00af4dcfe4b4242b5f19
Nikesh Maharjan <nikeshmhr@gmail.com>
2024-04-10 21:33:35 +0545

    minor fix.

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export default function NotFound() {
     return (
-        <Box className="shadow-inner border w-full h-20 md:h-96 flex justify-center items-center text-slate-300 text-2xl">
+        <Box className="shadow-inner border max-w-full min-w-fit h-20 md:h-96 flex justify-center items-center text-slate-300 text-2xl p-8">
             Nothing to show
         </Box>
     )
